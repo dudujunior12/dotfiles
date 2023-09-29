9c55ca549cbd2380b41d2fbd7d29df0346f519c1
Eduardo dos Santos <dudu12310@hotmail.com>
2023-09-29 13:16:39 -0300

    feat: nvim transparency

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,6 +1,9 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'tokyonight'}
+ M.ui = {
+  theme = 'tokyonight',
+  transparency = true,
+}
  M.plugins = "custom.plugins"
  M.mappings = require "custom.mappings"
  return M
