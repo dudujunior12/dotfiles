e9a4d22c7f9b1e3a8dd537a9834014f5b0670df5
Eduardo dos Santos <dudu12310@hotmail.com>
2024-05-14 14:36:27 -0300

    feat: fish files

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,43 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+fish_vi_key_bindings
+
+abbr n "nvim"
+abbr p "sudo pacman"
+abbr y "yay"
+abbr dcu "docker compose up"
+abbr dcd "docker compose down"
+abbr ga "git add"
+abbr gc "git commit -m"
+abbr gp "git push"
+abbr oci "oci --auth security_token"
+abbr firefox "firefox-nightly"
+abbr edge "microsoft-edge-stable"
+abbr vsi "sudo virsh start saber"
+abbr vss "sudo virsh shutdown saber"
+abbr sshs "kitty +kitten ssh -X ejunior@saber"
+
+bind \e\[1\;3D prevd
+bind \e\[1\;3C nextd
+#source $HOME/work/.config/fish/abbr.fish
+alias cat="bat"
+alias ls="exa"
+alias ssh="kitty +kitten ssh -X"
+# alias ssha="kitty +kitten ssh -X ejunior@saber"
+alias reflector="sudo reflector --latest 5 --sort rate --protocol https --country Brazil --country US --country POrtugal --country Japan --save /etc/pacman.d/mirrorlist"
+
+set fzf_preview_dir_cmd exa --all --color=always
+set fzf_fd_opts -uu -i -L --exclude=.git/
+fzf_configure_bindings --directory=\cf --variables
+
+set -U Z_CMD "j"
+set -U ZO_METHOD "ranger"
+
+set -U EDITOR nvim
+set -U VISUAL nvim
+
+# pyenv configuration
+# status is-login; and pyenv init --path | source
+# status is-interactive; and pyenv init - | source
+# status is-interactive; and pyenv virtualenv-init - | source
+# end pyenv configuration
+set -x GPG_TTY (tty)
+fish_add_path /home/anscient/.spicetify
